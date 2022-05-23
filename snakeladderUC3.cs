9fda1320adf058d97617e85b6a63088a4a7f7a81
milanbiranwar <102604173+milanbiranwar@users.noreply.github.com>
2022-05-23 06:09:32 +0530

    Update snakeladderUC3.cs

--- snakeladderUC3.cs
+++ snakeladderUC3.cs
@@ -10,34 +10,42 @@ namespace snakeandladder
     {
         public static void UC3()
         { 
-          int position = 0;
-          Console.WriteLine("position is " + position);
-
-
+           int position = 0;
+            
+            //Random number for rolldice
             Random obj = new Random();
             int rollDice = obj.Next(1, 7);
             Console.WriteLine("Dice Value: " + rollDice);
 
+            //Random numbers for position
             Random num = new Random();
             int value = num.Next(1, 4);
             Console.WriteLine("case number is " + value);
 
-                switch (value)
-                {
-                    case 1:
-                    Console.WriteLine("No Play You are in the same position.");
-                        break;
+            
+            if (value == 1)  
+            {
+           
+               Console.WriteLine("You got NO PLAY Option");
 
-                    case 2:
-                        position = (position + rollDice);
-                    Console.WriteLine("your position after ladder is: " + position);
-                        break;
+            }
+            else if (value == 2)
+            {
+              Console.WriteLine("You got Ladder Option");
+               
+                position = position + rollDice;
+                Console.WriteLine(" your position: {0}", position);
 
-                    case 3:
-                        position = (position - rollDice);
-                    Console.WriteLine("Your Position after snake is: " + position);
-                        break;
-                }
+            }
+            else
+            {
+                Console.WriteLine("You got Snake Option");
+
+                
+                position = position - rollDice;
+                Console.WriteLine("You got snake \n your position: {0} \n ", position);
+
+            }
         }
     }
 }
