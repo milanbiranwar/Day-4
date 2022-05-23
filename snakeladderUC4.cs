3bf6cbf09850c7d1766ed975eb461d2c00ade7c0
milanbiranwar <102604173+milanbiranwar@users.noreply.github.com>
2022-05-23 06:11:59 +0530

    Update snakeladderUC4.cs

--- snakeladderUC4.cs
+++ snakeladderUC4.cs
@@ -8,46 +8,47 @@ namespace snakeandladder
 {
     internal class snakeladderUC4
     {
-        public static void UC4()
+        public static int dicecount = 0;
+        public static int num1;
+        public static void UC3()
         {
             int position = 0;
-            Console.WriteLine("position is " + position);
-
-
+            
+            //Random number for rolldice
             Random obj = new Random();
             int rollDice = obj.Next(1, 7);
             Console.WriteLine("Dice Value: " + rollDice);
 
+            //Random numbers for position
             Random num = new Random();
             int value = num.Next(1, 4);
             Console.WriteLine("case number is " + value);
 
-            while (position <= 99)
+            
+            if (value == 1)  
             {
+           
+               Console.WriteLine("You got NO PLAY Option");
 
-                Random var = new Random();
-                int optionValue = var.Next(1, 4);
-
-                switch (optionValue)
-                {
-                    case 1:
-                        Console.WriteLine("No Play You are in the same position");
-                        break;
-                    case 2:
-                        position = (position + rollDice);
-                        Console.WriteLine("your position after ladder is: " + position);
-                        break;
-
-                    case 3:
-                        position = (position - rollDice);
-                        if (position < 0)
-                        {
-                            position = 0;
-                        }
-                        Console.WriteLine("Your Position after snake is: " + position);
-                        break;
-                }
             }
+            else if (value == 2)
+            {
+              Console.WriteLine("You got Ladder Option");
+               
+                position = position + rollDice;
+                Console.WriteLine(" your position: {0}", position);
+
+            }
+            else
+            {
+                Console.WriteLine("You got Snake Option");
+
+                
+                position = position - rollDice;
+                Console.WriteLine("You got snake \n your position: {0} \n ", position);
+
+            }
+
         }
     }
 }
